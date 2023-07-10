c9123b65928b3f3d364596184874340f274cc798
Joshua <kenduajayi@gmail.com>
2023-07-10 02:52:07 -0700

    Parv: Write a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""6-base_geometry module file"""
+
+
+class BaseGeometry:
+    """the BaseGeometry class"""
+    def area(self):
+        """this is a public instance method that raises an exception"""
+        raise Exception("area() is not implemented")
