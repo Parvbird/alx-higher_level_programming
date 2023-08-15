650e46d86cc19cc303978aca484cd688259ee823
Joshua <kenduajayi@gmail.com>
2023-08-15 11:09:54 -0700

    Parv: modified file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,10 @@
--- The script that creates a table second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
-
-INSERT INTO second_table (id, name, score)
+-- creates table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES
-	(1, "Jane", 10),
-	(2, "Tom", 3),
-	(3, "Joshua", 14),
-	(4, "Kelvin", 8);
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
