092a34b12d78150a59cbc65bc5dd8736d3b279b7
Joshua <kenduajayi@gmail.com>
2023-08-16 12:48:29 -0700

    Parv: show linked genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all genres contained in hbtn_0d_tvshow
+-- and displays numbers of shows that are linked
+SELECT g.`name` AS `genre`, COUNT(sg.show_id) AS `number_of_shows`
+FROM tv_genres AS g
+LEFT JOIN tv_show_genres AS sg ON g.`id` = sg.`genre_id`
+GROUP BY g.`name`
+HAVING `number_of_shows` > 0
+ORDER BY `number_of_shows` DESC;
