65f8cf9c30032d696f0c1b45615673eaac77efb7
Joshua <kenduajayi@gmail.com>
2023-08-16 12:41:06 -0700

    Parv: modified file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,6 +5,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- This would grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- This would reload grant tables
-FLUSH PRIVILEGES;
