793d0fb206c5bda26e87ef79ae285b15cfbbed6a
Joshua <kenduajayi@gmail.com>
2023-08-16 07:48:41 -0700

    Parv: script that creates the MySQL server user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+-- The script that creates a MySQL server user
+
+-- This would create a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- This would grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- This would reload grant tables
+FLUSH PRIVILEGES;
