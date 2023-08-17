5dd9dcd2aac9269aedaaa5680d11a855ab6fb043
Joshua <kenduajayi@gmail.com>
2023-08-17 12:40:33 -0700

    Parv: remodified

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,12 @@
+-- This lists all Comedy shows in the database hbtn_0d_tvshows
+-- and displays the number of shows linked to each
+   -- Each record should display the:
+      -- tv_shows.title
+   -- The results must be sorted in ascending order by show title
+   -- The database name will be passed as argument of the mysql command
+
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
