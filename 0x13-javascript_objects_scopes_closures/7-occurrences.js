a6d2ed20a484ab8d0579d49e62e8430cdc202c17
Joshua Ajayi <kenduajayi@gmail.com>
2023-09-12 12:31:06 -0700

    Parv: new file

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Function that computes the number of occurence of an item in a list
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
