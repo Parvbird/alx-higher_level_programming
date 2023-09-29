2d213175dfd74d4b55c3e89b38703534e6b84a2e
Joshua Ajayi <kenduajayi@gmail.com>
2023-09-29 08:59:09 -0700

    Parv: a Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to
+- take in URL
+- send a request to URL
+- display body of response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
+
