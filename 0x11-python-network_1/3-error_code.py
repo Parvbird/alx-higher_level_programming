77f32b6ecc017ef3ad934b2409109595d5a653f5
Joshua Ajayi <kenduajayi@gmail.com>
2023-09-29 08:51:45 -0700

    Parv: a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to
+- take in URL,
+- send a request to URL
+- display body of response (decoded in utf-8).
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
+
